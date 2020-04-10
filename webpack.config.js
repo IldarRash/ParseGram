c1ee6acffbf8d7c62c2f03a5b3cac36d823e5c6d
tatarin <IShayakhmetov@alfabank.ru>
2020-04-10 11:36:17 +0300

    move to react

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    resolve: {
+        modules: [
+            path.join(__dirname, 'src', 'main', 'resources', 'js'),
+            path.join(__dirname, 'node_modules'),
+        ],
+        extensions: ['.js', '.jsx']
+    },
+    module: {
+        rules: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader?presets[]=es2016&presets[]=react'
+            }
+        ],
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: './src/main/resources/js/index.html'
+    })],
+    entry: path.join(__dirname, 'src', 'main', 'resources', 'js'),
+    devServer: {
+        historyApiFallback: true
+    },
+    externals: {
+        // global app config object
+        config: JSON.stringify({
+            apiUrl: 'http://localhost:4000'
+        })
+    }
+}
